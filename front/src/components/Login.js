dcb670a5f3fd2a97ca867e8cc823e1658839066f
Txxxthless <lepash85@gmail.com>
2023-04-29 16:28:05 +0300

    Form validation added

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -7,28 +7,56 @@ function Login() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const errors = {};
+    errors.canBeSend = true;
+    if (!name) {
+      errors.username = "Username is required";
+      errors.canBeSend = false;
+    }
+    if (!password) {
+      errors.password = "Password is required";
+      errors.canBeSend = false;
+    }
+    console.log(errors);
+    return errors;
+  };
+
   const login = () => {
     const loginViewModel = {
       name: name,
       password: password,
     };
 
-    axios
-      .post(apiEndpoint, loginViewModel)
-      .then((response) => {
-        const token = response.data.token;
-        localStorage.setItem("token", token);
-        const username = response.data.username;
-        localStorage.setItem("username", username);
-        window.location.href = "/";
-      })
-      .catch((error) => console.log(error));
+    const errors = validate();
+    setErrors(errors);
+
+    if (errors.canBeSend) {
+      axios
+        .post(apiEndpoint, loginViewModel)
+        .then((response) => {
+          if (response.status === 200) {
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            const username = response.data.username;
+            localStorage.setItem("username", username);
+            window.location.href = "/";
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          setErrors({ ...error, login: "Incorrect username or password" });
+        });
+    }
   };
 
   return (
     <div className="card log-reg-card">
       <div className="row">
         <h1 className="text-white">Login</h1>
+        <p className="chat-error-msg">{errors.login}</p>
         <h2 className="text-white">Name</h2>
         <input
           className="form-control log-reg-input form-control-lg"
@@ -36,6 +64,7 @@ function Login() {
           placeholder="Enter your name..."
           onChange={(event) => setName(event.target.value)}
         ></input>
+        <p className="chat-error-msg">{errors.username}</p>
         <h2 className="text-white">Password</h2>
         <input
           className="form-control log-reg-input form-control-lg"
@@ -43,6 +72,7 @@ function Login() {
           placeholder="Enter password..."
           onChange={(event) => setPassword(event.target.value)}
         ></input>
+        <p className="chat-error-msg">{errors.password}</p>
         <div>
           <button
             className="btn btn-light btn-lg align-text-bottom log-reg-btn"
--- front/src/components/Registration.js
+++ front/src/components/Registration.js
@@ -5,17 +5,50 @@ function Register() {
   const apiEndpoint = "http://localhost:5093/api/Auth/Register";
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const errors = {};
+
+    errors.canBeSend = true;
+
+    if (!name) {
+      errors.username = "Username is required";
+      errors.canBeSend = false;
+    } else if (name.length < 4) {
+      errors.username = "Username must be more than 3 characters";
+      errors.canBeSend = false;
+    }
+    if (!password) {
+      errors.password = "Password is required";
+      errors.canBeSend = false;
+    } else if (password !== confirmPassword) {
+      errors.confirmPassword = "Passwords don't match";
+      errors.canBeSend = false;
+    } else if (password.length < 4) {
+      errors.password = "Password must be more than 3 characters";
+      errors.canBeSend = false;
+    }
+    if (!confirmPassword) {
+      errors.confirmPassword = "Repeat the password";
+      errors.canBeSend = false;
+    }
+
+    return errors;
+  };
 
   const register = () => {
-    if (passwordRef.current.value === passwordConfirmRef.current.value) {
+    const errors = validate();
+    setErrors(errors);
+
+    if (errors.canBeSend) {
       const registerViewModel = {
         name: name,
         password: password,
       };
-      console.log(registerViewModel);
+
       axios
         .post(apiEndpoint, registerViewModel)
         .then((response) => {
@@ -26,8 +59,6 @@ function Register() {
           window.location.href = "/";
         })
         .catch((error) => console.log(error));
-    } else {
-      return;
     }
   };
 
@@ -42,21 +73,23 @@ function Register() {
           placeholder="Enter your name..."
           onChange={(event) => setName(event.target.value)}
         ></input>
+        <p className="chat-error-msg">{errors.username}</p>
         <h2 className="text-white">Password</h2>
         <input
           className="form-control log-reg-input form-control-lg"
           type="password"
           placeholder="Enter password..."
           onChange={(event) => setPassword(event.target.value)}
-          ref={passwordRef}
         ></input>
+        <p className="chat-error-msg">{errors.password}</p>
         <h2 className="text-white">Repeat password</h2>
         <input
           className="form-control log-reg-input form-control-lg"
           type="password"
           placeholder="Repeat password..."
-          ref={passwordConfirmRef}
+          onChange={(event) => setConfirmPassword(event.target.value)}
         ></input>
+        <p className="chat-error-msg">{errors.confirmPassword}</p>
         <div>
           <button
             className="btn btn-light btn-lg align-text-bottom log-reg-btn"
